09471cf300376f8e26802d3a831a0ff20e7c93ad
Abdurrahman Fi Zhilalil Islam <Abdurrahman.Fi@ai.astra.co.id>
2020-06-23 21:03:18 +0700

    create django project

---
+++ .djangodev/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\abdurrahman\scripts\develop\frameworkdjango\.djangodev\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
